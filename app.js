18bfffa0d2d42892e45d93b22deeaf430bffb673
rajesh1096 <125876142+rajesh1096@users.noreply.github.com>
2023-04-06 13:05:56 +0530

    update app.js
    removed sshhh

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   // Send the response body as "Hello Rajessssshhhhhh.....!"
-   response.end('Hello Rajesssssshhhhhhhh.....!\n');
+   // Send the response body as "Hello Rajesh.....!"
+   response.end('Hello Rajesh....!\n');
 }).listen(8081);
 
 // Console will print the message
